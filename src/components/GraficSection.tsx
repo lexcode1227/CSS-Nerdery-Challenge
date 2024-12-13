347dcbef5132733491eb1178e29a80650aabab98
Henry Agustin <henryagustin297@gmail.com>
2024-12-12 18:06:52 -0600

    style(right sidebar section): add styles to the right sidebar

--- src/components/GraficSection.tsx
+++ src/components/GraficSection.tsx
@@ -6,11 +6,11 @@ const GraficSection = () => {
         <div className='containerTop-User'>
             <div>
                 <div className='box-icons'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                     </svg>
                 </div>
                 <div className='box-user'>
@@ -23,36 +23,37 @@ const GraficSection = () => {
         </div>
         <div className='container-stadistics'>
             <h3>Storage</h3>
-            <div className='stadistic-grafic'>
-                fg
+            <div className='subContainer-stadistic'>
+                <div className='stadistic-grafic'></div>
+                <p>420.2 GB of 500 GB used</p>
             </div>
             <div className='box-stadistic'>
                 <article className='card-stadistic'>
-                    <div className='card-stadistic-square'>d</div>
+                    <div className='card-stadistic-square'></div>
                     <div className='card-stadistic-container'>
                         <h4>Documents</h4>
                         <p>7200 files</p>
                     </div>
-                    <p>200 GB</p>
+                    <p>125 GB</p>
                 </article>
                 <article className='card-stadistic'>
-                    <div className='card-stadistic-square'>d</div>
+                    <div className='card-stadistic-square'></div>
                     <div className='card-stadistic-container'>
                         <h4>Documents</h4>
                         <p>7200 files</p>
                     </div>
-                    <p>200 GB</p>
+                    <p>75 GB</p>
                 </article>
                 <article className='card-stadistic'>
-                    <div className='card-stadistic-square'>d</div>
+                    <div className='card-stadistic-square'></div>
                     <div className='card-stadistic-container'>
                         <h4>Documents</h4>
                         <p>7200 files</p>
                     </div>
-                    <p>200 GB</p>
+                    <p>50 GB</p>
                 </article>
                 <article className='card-stadistic'>
-                    <div className='card-stadistic-square'>d</div>
+                    <div className='card-stadistic-square'></div>
                     <div className='card-stadistic-container'>
                         <h4>Documents</h4>
                         <p>7200 files</p>
